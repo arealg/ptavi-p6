010c8927b9c497165d873e56f6a63f77333d44b9
Ana Real <anavm4_@hotmail.com>
2015-11-23 16:51:06 +0100

    Actualizacion

--- client.py
+++ client.py
@@ -10,31 +10,28 @@ import sys
 if len(sys.argv) != 3:
     sys.exit('Usage: python3 client.py method receiver@IP:SIPport')
 
-
 server_port = sys.argv[2].split('@')
 
 METODO = sys.argv[1]
 LOGIN = sys.argv[2].split(':')[0]
 IP = server_port[1].split(':')[0]
 PORT = int(server_port[1].split(':')[1])
-
-
-# Contenido que vamos a enviar
 LINE = METODO + ' ' + 'sip:' + LOGIN + ' ' + 'SIP/2.0'
-print(LINE)
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((IP, PORT))
 
-print("Enviando: " + LINE)
 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
 data = my_socket.recv(1024)
 
-print('Recibido -- ', data.decode('utf-8'))
-print("Terminando socket...")
+print(data.decode('utf-8'))
+ack_msg = data.decode('utf-8')
 
-# Cerramos todo
+if ('100 Trying' in ack_msg and '180 Ring' in ack_msg
+    and '200 OK' in ack_msg):
+    my_socket.send(bytes('ACK' + ' ' + 'sip:' + LOGIN + ' '
+                   + 'SIP/2.0', 'utf-8')
+                   + b'\r\n')
+
 my_socket.close()
-print("Fin.")
